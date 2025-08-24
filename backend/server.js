78ac1537d020cd6c85c47d7359d0eaca68f6bc98
dineshchandrasekaren <dineshchandrasekaren@gmail.com>
2025-08-24 15:56:02 +0530

    serve front end only in production

--- backend/server.js
+++ backend/server.js
@@ -13,13 +13,13 @@ const PORT = process.env.PORT || "5000";
 
 app.use("/api", route);
 
-// if(process.env.NODE_ENV==="production"){
+if(process.env.NODE_ENV==="production"){
   app.use(static_(path.join(__dirname, "/frontend/dist")));
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "/frontend/dist/index.html"));
   });
 
-// }
+}
 
 app.listen(PORT, () => {
   console.log(`server is running in port ${PORT}`);
